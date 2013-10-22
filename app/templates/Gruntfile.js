3a390340e723968af595d5c7b46a7ae56a251b9d
Ben McGinnis <mcginnisb@gmail.com>
2013-10-22 19:25:20 -0400

    Super simple LESS compiler grunt task

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -1,2 +1,20 @@
 'use strict';
 
+module.exports = function(grunt)
+{
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // load all grunt tasks
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          strictImports: true
+        },
+        files: {
+          "css/style.css": "less/style.less"
+        }
+      }
+    }
+  });
+}
